2aa10ff9508ff35f134d27b95754154314a89592
Jiannan Guo <guojiannan1101@gmail.com>
2013-11-24 06:49:43 +0100

    control functions

---
+++ EP2300-Makeup-CodeTemplate/src/peersim/EP2300/control/GAPTreeInitializer.java
@@ -0,0 +1,13 @@
+package peersim.EP2300.control;
+
+import peersim.core.Control;
+
+public class GAPTreeInitializer implements Control {
+
+	@Override
+	public boolean execute() {
+		// TODO Initialize root/non-root nodes with proper vector
+		return false;
+	}
+
+}
---
+++ EP2300-Makeup-CodeTemplate/src/peersim/EP2300/control/ResetMsgBudget.java
@@ -0,0 +1,13 @@
+package peersim.EP2300.control;
+
+import peersim.core.Control;
+
+public class ResetMsgBudget implements Control {
+
+	@Override
+	public boolean execute() {
+		// TODO Reset message budget to r for next cycle
+		return false;
+	}
+
+}
