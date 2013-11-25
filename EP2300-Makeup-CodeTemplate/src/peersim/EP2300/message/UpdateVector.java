8deac02c44c420f83a1b529a35d3d325ff03355e
Jiannan Guo <guojiannan1101@gmail.com>
2013-11-25 17:06:51 +0100

    Message between nodes

--- EP2300-Makeup-CodeTemplate/src/peersim/EP2300/message/UpdateVector.java
+++ EP2300-Makeup-CodeTemplate/src/peersim/EP2300/message/UpdateVector.java
@@ -1,16 +1,18 @@
 package peersim.EP2300.message;
 
+import peersim.core.Node;
+
 /*
  * message structure to be sent among nodes
  */
 public class UpdateVector {
-	final public double id;
+	final public Node sender;
 	final public double level;
 	final public double parent;
 	final public long aggregate;
 
-	public UpdateVector(double id, double level, double parent, long aggregate) {
-		this.id = id;
+	public UpdateVector(Node sender, double level, double parent, long aggregate) {
+		this.sender = sender;
 		this.level = level;
 		this.parent = parent;
 		this.aggregate = aggregate;
