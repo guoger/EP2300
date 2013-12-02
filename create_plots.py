af171173580575c109ca5d2159059e26b84dd088
Niklas Semmler <metaswirl@gmail.com>
2013-12-02 19:06:50 +0100

    improved plotting

--- create_plots.py
+++ create_plots.py
@@ -9,7 +9,7 @@ import argparse
 from scipy.stats import gaussian_kde
 import os
 
-MARKER_SIZE=5
+MARKER_SIZE=4
 ALPHA_VAL=0.7
 WARM_UP_PERIOD = 60
 PLOT_PATH = "report/plots"
@@ -17,13 +17,21 @@ DEFAULT_PATH = "EP2300-Makeup-CodeTemplate/sim-results/out/result.log"
 NUM_PDF_VALUES = 20
 NAMES_LIST = ['index', 'actMax', 'estMax', 'estMaxErr', 'actAvg', 'estAvg',
     'estAvgErr', 'maxMsgRate', 'overhead']
+
+def get_color():
+    index = -1
+    colors = ['bx', 'go', 'rs', 'cd', 'mD', 'y', 'k', 'w']
+    while index+1 < len(colors):
+        index += 1
+        yield colors[index]
+
 params = {
-    'actMax':(['rx'], {'label':'act max', 'alpha':0.5, 'markersize':3}),
-    'estMax':(['yo'], {'label':'est max', 'alpha':0.5, 'markersize':3}),
-    'estMaxErr':(['gs'], {'label':'est max err', 'markersize':5}),
-    'actAvg':(['y-'], {'label':'act avg', 'alpha':0.5, 'linewidth':5, 'markersize':5}),
-    'estAvg':(['m-'], {'label':'est avg', 'linewidth':1, 'markersize':5}),
-    'estAvgErr':(['cd'], {'label':'est avg err', 'markersize':5})
+    'actMax':(['x-'], {'markersize':MARKER_SIZE}),
+    'estMax':(['o-'], {'linewidth':2, 'markersize':MARKER_SIZE}),
+    'estMaxErr':(['s'], {'markersize':MARKER_SIZE}),
+    'actAvg':(['-'], {'alpha':0.5, 'linewidth':5, 'markersize':MARKER_SIZE}),
+    'estAvg':(['-'], {'linewidth':2, 'markersize':MARKER_SIZE}),
+    'estAvgErr':(['d'], {'markersize':MARKER_SIZE})
     #'maxMsgRate':(['b+'], {})
 }
 names = {}
@@ -33,32 +41,43 @@ for name in NAMES_LIST:
     names[name] = i
     i += 1
 
-def time_series_plot(ax, data, variables):
+def time_series_plot(ax, data, metadata, val):
+    est = 'est' + val
+    act = 'act' + val
     ax.grid(True)
-    time = data['index']
-    for var in variables:
-        plt.plot(time, data[var], *params[var][0], **params[var][1])
-    plt.xlabel("time")
+    virgin = True
+    for name, datum in data.iteritems():
+        if virgin:
+            time = datum['index']
+            plt.plot(time[0:300], datum[act][0:300], *params[act][0], label=act, **params[act][1])
+            virgin = False
+        plt.plot(time[0:300], datum[est][0:300], metadata[name] + '--', label=name, **params[est][1])
+        plt.xlabel("time")
+        plt.xlabel(val)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, labels, numpoints=1)
 
-def trade_off_plot(ax, data, variables):
+def trade_off_plot(ax, data, metadata, val):
     ax.grid(True)
-    overhead = data['overhead']
-    for var in variables:
-        plt.plot(data[var], overhead, *params[var][0], **params[var][1])
+    err = 'est' + val + 'Err'
+    for name, datum in data.iteritems():
+        overhead = datum['overhead']
+        plt.plot(datum[err], overhead, metadata[name], label=name, **params[err][1])
     plt.ylabel("overhead")
+    plt.xlabel(err)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, labels, numpoints=1)
 
-def density_plot(ax, data, variables):
+def density_plot(ax, data, metadata, val):
+    err = 'est' + val + 'Err'
     ax.grid(True)
-    for var in variables:
-        data_wo_warmup = data[var][WARM_UP_PERIOD:]
-        density = gaussian_kde(data_wo_warmup)
-        rangex = np.arange(min(data_wo_warmup), max(data_wo_warmup), NUM_PDF_VALUES)
-        plt.plot(rangex, density(rangex), *params[var][0], **params[var][1])
-    plt.ylabel("pdf")
+    for name, datum in data.iteritems():
+        datum_wo_warmup = datum[err][WARM_UP_PERIOD:]
+        density = gaussian_kde(datum_wo_warmup)
+        rangex = np.arange(min(datum_wo_warmup), max(datum_wo_warmup), NUM_PDF_VALUES)
+        plt.plot(rangex, density(rangex), metadata[name] + '-', label=name, **params[err][1])
+        plt.ylabel("pdf")
+        plt.xlabel(err)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, labels, numpoints=1)
 
@@ -71,55 +90,47 @@ def parse_args(options):
 def read_data(input_file):
     data = np.genfromtxt(input_file, delimiter=' ')
     data2 = {}
-    data2['index'] = data[:, names['index']]
-    data2['actMax'] = data[:, names['actMax']]
-    data2['actAvg'] = data[:, names['actAvg']]
-    data2['overhead'] = data[:, names['overhead']]
-    data2['estMaxErr'] = data[:, names['estMaxErr']]
-    data2['estAvgErr'] = data[:, names['estAvgErr']]
-    data2['estMax'] = data[:, names['estMax']]
-    data2['estAvg'] = data[:, names['estAvg']]
+    for sid in NAMES_LIST:
+        data2[sid] = data[:, names[sid]]
     return data2
 
-def draw_plots(data):
+def read_data_for(task, pattern):
+    tasks = ['task1', 'task2', 'task3']
+    patterns = ['R1', 'R2']
+    all_fnames = []
+    def get_fnames(store, dirname, fnames):
+        store += fnames
+
+    directory = os.path.join('data', tasks[task-1], patterns[pattern-1])
+    os.path.walk(directory, get_fnames, all_fnames)
+    data = {}
+    metadata = {}
+    name = ""
+    color = get_color()
+    for fname in all_fnames:
+        name = fname.split('.')[0].replace('_', '.')
+        data[name] = read_data(os.path.join(directory, fname))
+        metadata[name] = color.next()
+
+    return data, metadata
+
+def draw_plots(data, metadata):
     fig = plt.figure(1)
     fig.suptitle("time series")
-    ax = fig.add_subplot(311)
-    time_series_plot(ax, data,
-        ["estMax", "actMax"]
-    )
-    ax = fig.add_subplot(312)
-    time_series_plot(ax, data,
-        ["estAvg", "actAvg"]
-    )
-    ax = fig.add_subplot(313)
-    time_series_plot(ax, data,
-        ["estMax", "actMax", "estAvg", "actAvg"]
-    )
+    ax = fig.add_subplot(111)
+    time_series_plot(ax, data, metadata, 'Max')
     fig.savefig(os.path.join(PLOT_PATH, "time_series.png"))
 
     fig = plt.figure(2)
     fig.suptitle("trade off")
-    ax = fig.add_subplot(211)
-    trade_off_plot(ax, data,
-        ["estMaxErr"]
-    )
-    ax = fig.add_subplot(212)
-    trade_off_plot(ax, data,
-        ["estAvgErr"]
-    )
+    ax = fig.add_subplot(111)
+    trade_off_plot(ax, data, metadata, "Max")
     fig.savefig(os.path.join(PLOT_PATH, "trade_off.png"))
 
     fig = plt.figure(3)
     fig.suptitle("density")
-    ax = fig.add_subplot(221)
-    density_plot(ax, data,
-        ["estMaxErr" ]
-    )
-    ax = fig.add_subplot(222)
-    density_plot(ax, data,
-        ["estAvgErr"]
-    )
+    ax = fig.add_subplot(111)
+    density_plot(ax, data, metadata, 'Max')
     fig.savefig(os.path.join(PLOT_PATH, "pdf_of_error.png"))
 
 def main():
@@ -128,10 +139,14 @@ def main():
             'help':"locate the log file"}),
         ('-s', {'action':"store_true", 'dest':"only_save",
             'help':"set if you want to save instead of show"}),
+        ('-t', {'dest':"task", 'type':int, 'required':True,
+            'help':"choose task to plot"}),
+        ('-p', {'dest':"pattern", 'type':int, 'required':False,
+            'help':"choose to plot"}),
     ]
     args = parse_args(options)
-    data = read_data(args.input_file or DEFAULT_PATH)
-    draw_plots(data)
+    data, metadata = read_data_for(args.pattern or 1, args.task)
+    draw_plots(data, metadata)
 
     if not args.only_save:
         plt.show()
